4b29c78e904d0a688ff4b1fbf8f752089fda16b0
suelen <suelen_barboza99@hotmail.com>
2026-05-19 00:06:00 -0300

    Adiciona estrutura do banco

---
+++ innovatech_db.sql
@@ -0,0 +1,458 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 19/05/2026 às 05:03
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `innovatech_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id` int(11) NOT NULL,
+  `projeto_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `comentario` text NOT NULL,
+  `atualizado_em` datetime DEFAULT NULL,
+  `criado_em` datetime DEFAULT current_timestamp(),
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `prioridade_usuario`
+--
+
+CREATE TABLE `prioridade_usuario` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `projeto_id` int(11) NOT NULL,
+  `prioridade` enum('Baixa','Média','Alta') NOT NULL DEFAULT 'Média'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `projetos`
+--
+
+CREATE TABLE `projetos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(150) NOT NULL,
+  `descricao` text NOT NULL,
+  `categoria` enum('TCC','Pesquisa','Extensão','Pessoal','Outro') DEFAULT NULL,
+  `data_inicio` date NOT NULL,
+  `data_fim` date NOT NULL,
+  `prioridade` enum('Baixa','Média','Alta') DEFAULT 'Média',
+  `status` enum('Planejamento','Andamento','Pendente','Concluído') DEFAULT 'Planejamento',
+  `criador_id` int(11) NOT NULL,
+  `criado_em` timestamp NOT NULL DEFAULT current_timestamp(),
+  `arquivado` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `projeto_aluno`
+--
+
+CREATE TABLE `projeto_aluno` (
+  `id` int(11) NOT NULL,
+  `projeto_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `projeto_orientador`
+--
+
+CREATE TABLE `projeto_orientador` (
+  `id` int(11) NOT NULL,
+  `projeto_id` int(11) NOT NULL,
+  `professor_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `projeto_usuario`
+--
+
+CREATE TABLE `projeto_usuario` (
+  `id` int(11) NOT NULL,
+  `projeto_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `papel` enum('Criador','Aluno','Orientador') NOT NULL,
+  `prioridade` enum('Baixa','Média','Alta') DEFAULT NULL,
+  `arquivado` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `relatorios`
+--
+
+CREATE TABLE `relatorios` (
+  `id` int(11) NOT NULL,
+  `projeto_id` int(11) NOT NULL,
+  `aluno_id` int(11) NOT NULL,
+  `titulo` varchar(150) NOT NULL,
+  `descricao` text NOT NULL,
+  `criado_em` datetime DEFAULT current_timestamp(),
+  `professor_id` int(11) NOT NULL,
+  `status` enum('Novo Relatório','Respondido','Concluído') DEFAULT 'Novo Relatório',
+  `resposta` text DEFAULT NULL,
+  `respondido_em` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `resposta_relatorio`
+--
+
+CREATE TABLE `resposta_relatorio` (
+  `id` int(11) NOT NULL,
+  `relatorio_id` int(11) NOT NULL,
+  `respondente_id` int(11) NOT NULL,
+  `resposta` text NOT NULL,
+  `respondido_em` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `suporte_chamados`
+--
+
+CREATE TABLE `suporte_chamados` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `usuario_nome` varchar(100) NOT NULL,
+  `usuario_email` varchar(100) NOT NULL,
+  `assunto` varchar(150) NOT NULL,
+  `mensagem` text NOT NULL,
+  `resposta` text DEFAULT NULL,
+  `status` enum('aberto','respondido','concluido') DEFAULT 'aberto',
+  `data_abertura` datetime DEFAULT current_timestamp(),
+  `data_resposta` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tarefas`
+--
+
+CREATE TABLE `tarefas` (
+  `id` int(11) NOT NULL,
+  `projeto_id` int(11) NOT NULL,
+  `nome` varchar(150) NOT NULL,
+  `descricao` text DEFAULT NULL,
+  `responsavel_id` int(11) DEFAULT NULL,
+  `prioridade` enum('Baixa','Média','Alta') DEFAULT 'Média',
+  `status` enum('Planejamento','Em Andamento','Concluído') DEFAULT 'Planejamento',
+  `data_inicio` date DEFAULT NULL,
+  `data_fim` date DEFAULT NULL,
+  `arquivado` tinyint(1) DEFAULT 0,
+  `criado_em` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tarefa_usuario`
+--
+
+CREATE TABLE `tarefa_usuario` (
+  `id` int(11) NOT NULL,
+  `tarefa_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `prioridade` enum('Baixa','Média','Alta') DEFAULT 'Média',
+  `arquivado` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `tipo_solicitado` enum('Admin','Aluno','Professor','Coordenador') NOT NULL,
+  `aprovado` tinyint(1) DEFAULT 0,
+  `ativo` tinyint(1) DEFAULT 1,
+  `criado_em` timestamp NOT NULL DEFAULT current_timestamp(),
+  `tipo_usuario` enum('Admin','Aluno','Professor','Coordenador') DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `projeto_id` (`projeto_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Índices de tabela `prioridade_usuario`
+--
+ALTER TABLE `prioridade_usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `usuario_id` (`usuario_id`,`projeto_id`);
+
+--
+-- Índices de tabela `projetos`
+--
+ALTER TABLE `projetos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_projeto_criador` (`criador_id`);
+
+--
+-- Índices de tabela `projeto_aluno`
+--
+ALTER TABLE `projeto_aluno`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_pa_projeto` (`projeto_id`),
+  ADD KEY `fk_pa_usuario` (`usuario_id`);
+
+--
+-- Índices de tabela `projeto_orientador`
+--
+ALTER TABLE `projeto_orientador`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_po_projeto` (`projeto_id`),
+  ADD KEY `fk_po_professor` (`professor_id`);
+
+--
+-- Índices de tabela `projeto_usuario`
+--
+ALTER TABLE `projeto_usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uk_projeto_usuario` (`projeto_id`,`usuario_id`),
+  ADD KEY `fk_pu_usuario` (`usuario_id`);
+
+--
+-- Índices de tabela `relatorios`
+--
+ALTER TABLE `relatorios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `projeto_id` (`projeto_id`),
+  ADD KEY `aluno_id` (`aluno_id`);
+
+--
+-- Índices de tabela `resposta_relatorio`
+--
+ALTER TABLE `resposta_relatorio`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `relatorio_id` (`relatorio_id`),
+  ADD KEY `professor_id` (`respondente_id`);
+
+--
+-- Índices de tabela `suporte_chamados`
+--
+ALTER TABLE `suporte_chamados`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `tarefas`
+--
+ALTER TABLE `tarefas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `projeto_id` (`projeto_id`),
+  ADD KEY `responsavel_id` (`responsavel_id`);
+
+--
+-- Índices de tabela `tarefa_usuario`
+--
+ALTER TABLE `tarefa_usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tarefa_id` (`tarefa_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `prioridade_usuario`
+--
+ALTER TABLE `prioridade_usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `projetos`
+--
+ALTER TABLE `projetos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `projeto_aluno`
+--
+ALTER TABLE `projeto_aluno`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `projeto_orientador`
+--
+ALTER TABLE `projeto_orientador`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `projeto_usuario`
+--
+ALTER TABLE `projeto_usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `relatorios`
+--
+ALTER TABLE `relatorios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `resposta_relatorio`
+--
+ALTER TABLE `resposta_relatorio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `suporte_chamados`
+--
+ALTER TABLE `suporte_chamados`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tarefas`
+--
+ALTER TABLE `tarefas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tarefa_usuario`
+--
+ALTER TABLE `tarefa_usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`projeto_id`) REFERENCES `projetos` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `projetos`
+--
+ALTER TABLE `projetos`
+  ADD CONSTRAINT `fk_projeto_criador` FOREIGN KEY (`criador_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `projeto_aluno`
+--
+ALTER TABLE `projeto_aluno`
+  ADD CONSTRAINT `fk_pa_projeto` FOREIGN KEY (`projeto_id`) REFERENCES `projetos` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_pa_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `projeto_orientador`
+--
+ALTER TABLE `projeto_orientador`
+  ADD CONSTRAINT `fk_po_professor` FOREIGN KEY (`professor_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_po_projeto` FOREIGN KEY (`projeto_id`) REFERENCES `projetos` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `projeto_usuario`
+--
+ALTER TABLE `projeto_usuario`
+  ADD CONSTRAINT `fk_pu_projeto` FOREIGN KEY (`projeto_id`) REFERENCES `projetos` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_pu_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `relatorios`
+--
+ALTER TABLE `relatorios`
+  ADD CONSTRAINT `relatorios_ibfk_1` FOREIGN KEY (`projeto_id`) REFERENCES `projetos` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `relatorios_ibfk_2` FOREIGN KEY (`aluno_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `resposta_relatorio`
+--
+ALTER TABLE `resposta_relatorio`
+  ADD CONSTRAINT `resposta_relatorio_ibfk_1` FOREIGN KEY (`relatorio_id`) REFERENCES `relatorios` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `resposta_relatorio_ibfk_2` FOREIGN KEY (`respondente_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `tarefas`
+--
+ALTER TABLE `tarefas`
+  ADD CONSTRAINT `tarefas_ibfk_1` FOREIGN KEY (`projeto_id`) REFERENCES `projetos` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `tarefas_ibfk_2` FOREIGN KEY (`responsavel_id`) REFERENCES `usuarios` (`id`) ON DELETE SET NULL;
+
+--
+-- Restrições para tabelas `tarefa_usuario`
+--
+ALTER TABLE `tarefa_usuario`
+  ADD CONSTRAINT `tarefa_usuario_ibfk_1` FOREIGN KEY (`tarefa_id`) REFERENCES `tarefas` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `tarefa_usuario_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
